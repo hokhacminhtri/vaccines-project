cb534ff60e70dd9586871140a2f71fee27d474f6
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-06-01 11:36:35 +0700

    BE: getVaccineList & getVaccinePackageList API

--- server/src/controllers/vaccine.controller.js
+++ server/src/controllers/vaccine.controller.js
@@ -1,4 +1,6 @@
 const Vaccine = require('../models/mongoose/vaccines.model');
+const { mongoosePaginate } = require('../mongoose-paginate');
+const DEFAULT = require('../constants/default');
 
 exports.getAllVaccines = async (req, res) => {
   try {
@@ -8,5 +10,24 @@ exports.getAllVaccines = async (req, res) => {
   } catch (error) {
     console.error('getAllVaccines ERROR: ', error);
     return res.status(500).json([]);
+  }
+};
+
+exports.getVaccineList = async (req, res) => {
+  try {
+    let page = req.query?.page || 1;
+    const { select = '', sort = 'price' } = req.query;
+
+    const docs = await mongoosePaginate(
+      Vaccine,
+      {},
+      { page: Number(page), pageSize: DEFAULT.PAGE_SIZE },
+      { sort, select },
+    );
+
+    return res.status(200).json(docs);
+  } catch (error) {
+    console.error('Function getVaccines Error: ', error);
+    return res.status(500).json();
   }
 };
