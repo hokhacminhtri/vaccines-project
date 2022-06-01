7296bcd6ad2c03c7f34565df9bdeecf3c8f5ce99
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-06-01 11:36:58 +0700

    FE: feature to show vaccine & vaccine package list

--- client/src/apis/vaccinePackageApi.js
+++ client/src/apis/vaccinePackageApi.js
@@ -6,6 +6,9 @@ const vaccinePackageApi = {
   getAllPackages: ({ select = '' }) => {
     return axiosClient.get(`${URL}/all`, { params: { select } });
   },
+  getVaccinePackageList: ({ select = '', sort = 'price', page = 1 }) => {
+    return axiosClient.get(`${URL}/list`, { params: { select, sort, page } });
+  },
 };
 
 export default vaccinePackageApi;
---
+++ client/src/constants/default.js
@@ -0,0 +1,2 @@
+export const VACCINE_AVT_URL =
+  'https://res.cloudinary.com/dynonary/image/upload/v1650187901/metamarket/product-not-found.png';
