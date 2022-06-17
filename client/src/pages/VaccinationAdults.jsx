c548095bbd57f207fd48e3e4830d75ee30eeb51c
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-06-17 20:41:33 +0700

    FE: hot fix layout

--- client/src/pages/VaccinationAdults.jsx
+++ client/src/pages/VaccinationAdults.jsx
@@ -1,8 +1,4 @@
-import {Container,Grid} from '@mui/material';
-import VaccinationCard from '../components/VaccinationCard';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import usePageTitle from '../hooks/usePageTitle';
+import { Container, Grid } from '@mui/material';
 import image1 from '../assets/images/vc-adults01.jpg';
 import image2 from '../assets/images/vc-adults02.jpg';
 import image3 from '../assets/images/vc-adults03.jpg';
@@ -12,101 +8,116 @@ import image6 from '../assets/images/vc-adults06.jpg';
 import image7 from '../assets/images/vc-adults07.jpg';
 import image8 from '../assets/images/vc-adults08.jpg';
 import image9 from '../assets/images/vc-children07.jpg';
+import VaccinationCard from '../components/VaccinationCard';
+import usePageTitle from '../hooks/usePageTitle';
 
-export default function VaccinationChildren()
-{
-  usePageTitle("Tiêm chủng cho người lớn");
+export default function VaccinationChildren() {
+  usePageTitle('Tiêm chủng cho người lớn');
   return (
-    <>
-      <Header />
-      <Container sx={{paddingBottom:5}}>
-        <div class="row div-breckum">
-          <div class="col-xs-12">
-            <span typeof="v:Breadcrumb"><a href="/" rel="v:url" property="v:title">TRANG CHỦ</a> » <span class="breadcrumb_last">Tiêm Chủng Cho Người Lớn</span></span>
-          </div>
+    <Container sx={{ paddingBottom: 5 }}>
+      <div className="row div-breckum">
+        <div className="col-xs-12">
+          <span typeof="v:Breadcrumb">
+            <a href="/" rel="v:url" property="v:title">
+              TRANG CHỦ
+            </a>{' '}
+            » <span className="breadcrumb_last">Tiêm Chủng Cho Người Lớn</span>
+          </span>
         </div>
-        <h3 class="content-title">
-			    <span class="text-blue">TIÊM CHỦNG CHO NGƯỜI LỚN</span>
-		    </h3>
-        <p>
-        Theo thống kê của Tổ chức Y tế Thế giới WHO, mỗi năm có đến 600.000 người lớn tử vong vì những căn bệnh truyền nhiễm nguy hiểm có thể phòng ngừa bằng vắc xin. Trong khi đó,
-        người lớn lại là nhóm có tỷ lệ tiêm chủng thấp vì chưa quan tâm hoặc thiếu thông tin về việc tiêm chủng vắc xin cho người lớn. Điều này không chỉ ảnh hưởng đến sức khoẻ của
-        chính họ và đặc biệt là nguy cơ lây lan bệnh dịch sang cho trẻ em. Người lớn hãy chủ động tiêm phòng để bản thân phòng tránh các căn bệnh nguy hiểm, đồng thời bảo vệ sức khỏe
-        của gia đình và cộng đồng.
-        </p>
-        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
-          <Grid item xs={4}>
-            <VaccinationCard
+      </div>
+      <h3 className="content-title">
+        <span className="text-blue">TIÊM CHỦNG CHO NGƯỜI LỚN</span>
+      </h3>
+      <p>
+        Theo thống kê của Tổ chức Y tế Thế giới WHO, mỗi năm có đến 600.000
+        người lớn tử vong vì những căn bệnh truyền nhiễm nguy hiểm có thể phòng
+        ngừa bằng vắc xin. Trong khi đó, người lớn lại là nhóm có tỷ lệ tiêm
+        chủng thấp vì chưa quan tâm hoặc thiếu thông tin về việc tiêm chủng vắc
+        xin cho người lớn. Điều này không chỉ ảnh hưởng đến sức khoẻ của chính
+        họ và đặc biệt là nguy cơ lây lan bệnh dịch sang cho trẻ em. Người lớn
+        hãy chủ động tiêm phòng để bản thân phòng tránh các căn bệnh nguy hiểm,
+        đồng thời bảo vệ sức khỏe của gia đình và cộng đồng.
+      </p>
+      <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
+        <Grid item xs={4}>
+          <VaccinationCard
             avt={image1}
-            title='Vì sao người lớn cần tiêm vắc xin'
-            desc='VÌ SAO NGƯỜI LỚN CẦN TIÊM VẮC XIN?
+            title="Vì sao người lớn cần tiêm vắc xin"
+            desc="VÌ SAO NGƯỜI LỚN CẦN TIÊM VẮC XIN?
             Vắc xin không chỉ cần thiết cho trẻ em mà còn rất quan trọng với người lớn.
-            Vì sao người lớn cần tiêm vắc xin? Những loại vắc xin nào dành cho người lớn?'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image2}
-            title='Lịch tiêm chủng cho tuổi vị thành niên và người lớn'
-            desc='Dựa trên thực tiễn dịch tễ và đặc điểm sức khỏe người trưởng thành tại Việt Nam,
-            các chuyên gia khuyến cáo lịch tiêm chủng cho tuổi vị thành niên và người lớn như sau.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image3}
-            title='Những điều cần biết trước khi tiêm'
-            desc='Những điều cần biết trước khi tiêm chủng giúp phát hiện bất thường,
-            tạm hoãn tiêm hoặc không tiêm một loại vắc xin nào đó, đảm bảo an toàn cho người được tiêm chủng.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image4}
-            title='Những điều cần biết sau khi tiêm'
-            desc='Hướng dẫn chi tiết những điều cần biết sau khi tiêm chủng,
-            cách chăm sóc khoa học và phương pháp theo dõi sau tiêm giúp đảm bảo an toàn tiêm chủng cho người được tiêm.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image5}
-            title='Các loại vắc xin cho người lớn'
-            desc='Theo tình hình dịch tễ và đặc điểm sức khỏe người trưởng thành,
-            các chuyên gia đưa ra khuyến cáo các loại vắc xin cho người lớn, trong đó có 6 loại đặc biệt ưu tiên.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image6}
-            title='Gói vắc xin cho trẻ vị thành niên và thanh niên'
-            desc='Chuyên gia khuyến cáo, việc tiêm chủng cũng như tiêm nhắc các vắc xin cho tuổi vị thành niên và thanh niên vô cùng quan trọng để phòng ngừa dịch bệnh nguy hiểm.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image7}
-            title='Gói vắc xin cho người trưởng thành'
-            desc='Gói vắc xin cho người trưởng thành gồm nhiều loại vắc xin quan trọng được các chuyên gia khuyến cáo nên tiêm càng sớm càng tốt để phòng các bệnh truyền nhiễm nguy hiểm.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image8}
-            title='Gói vắc xin cho phụ nữ chuẩn bị mang thai và mang thai'
-            desc='Gói vắc xin cho phụ nữ chuẩn bị mang thai và mang thai gồm những vắc xin cần thiết giúp bảo vệ mẹ và bé phòng ngừa các bệnh truyền nhiễm nguy hiểm trước, trong thai kỳ.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image9}
-            title='Quy trình tiêm chủng'
-            desc='Quy trình tiêm chủng khoa học, khép kín gồm 5 bước, giúp khách hàng tiêm chủng an toàn, thuận tiện, nhanh chóng, tiết kiệm thời gian, không lo chờ đợi.'
-            link='/'/>
-          </Grid>
+            Vì sao người lớn cần tiêm vắc xin? Những loại vắc xin nào dành cho người lớn?"
+            link="/"
+          />
         </Grid>
-      </Container>
-      <Footer />
-    </>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image2}
+            title="Lịch tiêm chủng cho tuổi vị thành niên và người lớn"
+            desc="Dựa trên thực tiễn dịch tễ và đặc điểm sức khỏe người trưởng thành tại Việt Nam,
+            các chuyên gia khuyến cáo lịch tiêm chủng cho tuổi vị thành niên và người lớn như sau."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image3}
+            title="Những điều cần biết trước khi tiêm"
+            desc="Những điều cần biết trước khi tiêm chủng giúp phát hiện bất thường,
+            tạm hoãn tiêm hoặc không tiêm một loại vắc xin nào đó, đảm bảo an toàn cho người được tiêm chủng."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image4}
+            title="Những điều cần biết sau khi tiêm"
+            desc="Hướng dẫn chi tiết những điều cần biết sau khi tiêm chủng,
+            cách chăm sóc khoa học và phương pháp theo dõi sau tiêm giúp đảm bảo an toàn tiêm chủng cho người được tiêm."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image5}
+            title="Các loại vắc xin cho người lớn"
+            desc="Theo tình hình dịch tễ và đặc điểm sức khỏe người trưởng thành,
+            các chuyên gia đưa ra khuyến cáo các loại vắc xin cho người lớn, trong đó có 6 loại đặc biệt ưu tiên."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image6}
+            title="Gói vắc xin cho trẻ vị thành niên và thanh niên"
+            desc="Chuyên gia khuyến cáo, việc tiêm chủng cũng như tiêm nhắc các vắc xin cho tuổi vị thành niên và thanh niên vô cùng quan trọng để phòng ngừa dịch bệnh nguy hiểm."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image7}
+            title="Gói vắc xin cho người trưởng thành"
+            desc="Gói vắc xin cho người trưởng thành gồm nhiều loại vắc xin quan trọng được các chuyên gia khuyến cáo nên tiêm càng sớm càng tốt để phòng các bệnh truyền nhiễm nguy hiểm."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image8}
+            title="Gói vắc xin cho phụ nữ chuẩn bị mang thai và mang thai"
+            desc="Gói vắc xin cho phụ nữ chuẩn bị mang thai và mang thai gồm những vắc xin cần thiết giúp bảo vệ mẹ và bé phòng ngừa các bệnh truyền nhiễm nguy hiểm trước, trong thai kỳ."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image9}
+            title="Quy trình tiêm chủng"
+            desc="Quy trình tiêm chủng khoa học, khép kín gồm 5 bước, giúp khách hàng tiêm chủng an toàn, thuận tiện, nhanh chóng, tiết kiệm thời gian, không lo chờ đợi."
+            link="/"
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
--- client/src/pages/VaccinationChildren.jsx
+++ client/src/pages/VaccinationChildren.jsx
@@ -1,112 +1,119 @@
-import {Container,Grid} from '@mui/material';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import VaccinationCard from '../components/VaccinationCard';
-import usePageTitle from '../hooks/usePageTitle';
+import { Container, Grid } from '@mui/material';
 import image1 from '../assets/images/vc-children01.jpg';
 import image2 from '../assets/images/vc-children02.jpg';
-import image3 from '../assets/images/vc-children03.jpg'
-import image4 from '../assets/images/vc-children04.jpg'
-import image5 from '../assets/images/vc-children05.jpg'
-import image6 from '../assets/images/vc-children06.jpg'
-import image7 from '../assets/images/vc-children07.jpg'
-import image8 from '../assets/images/vc-children08.jpg'
+import image3 from '../assets/images/vc-children03.jpg';
+import image4 from '../assets/images/vc-children04.jpg';
+import image5 from '../assets/images/vc-children05.jpg';
+import image6 from '../assets/images/vc-children06.jpg';
+import image7 from '../assets/images/vc-children07.jpg';
+import image8 from '../assets/images/vc-children08.jpg';
+import VaccinationCard from '../components/VaccinationCard';
+import usePageTitle from '../hooks/usePageTitle';
 
-export default function VaccinationChildren()
-{
-  usePageTitle("Tiêm chủng cho trẻ em");
+export default function VaccinationChildren() {
+  usePageTitle('Tiêm chủng cho trẻ em');
   return (
-    <>
-      <Header />
-      <Container sx={{paddingBottom:5}}>
-        <div class="row div-breckum">
-          <div class="col-xs-12">
-            <span typeof="v:Breadcrumb"><a href="/" rel="v:url" property="v:title">TRANG CHỦ</a> » <span class="breadcrumb_last">Tiêm Chủng Cho Trẻ Em</span></span>
-          </div>
-        </div>
-        <h3 class="content-title">
-			    <span class="text-blue">
-            Tiêm Chủng Cho Trẻ Em
+    <Container sx={{ paddingBottom: 5 }}>
+      <div className="row div-breckum">
+        <div className="col-xs-12">
+          <span typeof="v:Breadcrumb">
+            <a href="/" rel="v:url" property="v:title">
+              TRANG CHỦ
+            </a>{' '}
+            » <span className="breadcrumb_last">Tiêm Chủng Cho Trẻ Em</span>
           </span>
-		    </h3>
-        <p>
-          Từ khi có vắc xin, Việt Nam đã bảo vệ hơn 1.6 triệu trẻ em khỏi gần 30 bệnh lý truyền nhiễm có thể gây tử vong;
-          thanh toán thủy đậu từ năm 1979; thanh toán bại liệt từ năm 2000;
-          thanh toán uốn ván sơ sinh từ năm 2005; tiết kiệm hàng ngàn tỷ đồng mỗi năm cho các chi phí chăm sóc y tế…
-          Chúng ta hoàn toàn có thể làm giảm tỷ lệ mắc bệnh, giảm biến chứng, giảm tử vong ở trẻ em dưới 5 tuổi do một số bệnh truyền nhiễm nguy hiểm gây ra nhờ chủng ngừa.
-          Hãy chủ động tiêm vắc xin để con yêu lớn lên an toàn và khỏe mạnh!
-        </p>
-        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
-          <Grid item xs={4}>
-            <VaccinationCard
+        </div>
+      </div>
+      <h3 className="content-title">
+        <span className="text-blue">Tiêm Chủng Cho Trẻ Em</span>
+      </h3>
+      <p>
+        Từ khi có vắc xin, Việt Nam đã bảo vệ hơn 1.6 triệu trẻ em khỏi gần 30
+        bệnh lý truyền nhiễm có thể gây tử vong; thanh toán thủy đậu từ năm
+        1979; thanh toán bại liệt từ năm 2000; thanh toán uốn ván sơ sinh từ năm
+        2005; tiết kiệm hàng ngàn tỷ đồng mỗi năm cho các chi phí chăm sóc y tế…
+        Chúng ta hoàn toàn có thể làm giảm tỷ lệ mắc bệnh, giảm biến chứng, giảm
+        tử vong ở trẻ em dưới 5 tuổi do một số bệnh truyền nhiễm nguy hiểm gây
+        ra nhờ chủng ngừa. Hãy chủ động tiêm vắc xin để con yêu lớn lên an toàn
+        và khỏe mạnh!
+      </p>
+      <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
+        <Grid item xs={4}>
+          <VaccinationCard
             avt={image1}
-            title='Lịch tiêm chủng cho trẻ'
-            desc='Cập nhật mới và đầy đủ nhất lịch tiêm chủng cho trẻ em,
+            title="Lịch tiêm chủng cho trẻ"
+            desc="Cập nhật mới và đầy đủ nhất lịch tiêm chủng cho trẻ em,
             đảm bảo bé yêu được chủng ngừa sớm, đầy đủ, đúng lịch,
-            bảo vệ bé trước nhiều bệnh truyền nhiễm nguy hiểm.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image2}
-            title='Những điều cần biết trước khi tiêm'
-            desc='Những điều cần biết trước khi tiêm giúp bác sĩ phát hiện bất thường,
-            tạm hoãn tiêm hoặc không tiêm một loại vắc xin nào đó,
-            đảm bảo an toàn cho trẻ khi đi tiêm chủng.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image3}
-            title='Những điều cần biết sau khi tiêm'
-            desc='Hướng dẫn chi tiết những điều cần biết sau khi tiêm chủng,
-            những phương pháp giúp phụ huynh theo dõi và chăm sóc trẻ sau tiêm,
-            đảm bảo an toàn tiêm chủng cho trẻ.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image4}
-            title='Các loại vắc xin cho trẻ em'
-            desc='VNVC có đủ các loại vắc xin cho trẻ em, được nhập khẩu từ nước ngoài của các hãng sản xuất uy tín,
-            nổi tiếng Thế giới, và số ít các vắc xin được sản xuất tại Việt Nam.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image5}
-            title='Gói vắc xin cho trẻ em'
-            desc='Gói vắc xin cho trẻ em đảm bảo có đầy đủ những loại vắc xin đã cam kết trong gói - ngay cả khi tình trạng khan hiếm vắc xin xảy ra,
-            cùng nhiều quyền lợi đặc biệt khác.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image6}
-            title='Gói vắc xin cho trẻ tiền học đường'
-            desc='Gói vắc xin cho trẻ tiền học đường giúp bố mẹ chủ động phòng ngừa nhiều bệnh truyền nhiễm nguy hiểm cho trẻ,
-            giúp trẻ được bảo vệ tối đa và phát triển toàn diện.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image7}
-            title='Quy trình tiêm chủng tại VNVC'
-            desc='Quy trình tiêm chủng tại VNVC khoa học, khép kín gồm 5 bước, giúp khách hàng tiêm chủng an toàn,
-            thuận tiện, nhanh chóng, tiết kiệm thời gian, không lo chờ đợi.'
-            link='/'/>
-          </Grid>
-          <Grid item xs={4}>
-            <VaccinationCard
-            avt={image8}
-            title='Bảng giá vắc xin'
-            desc='Cập nhật bảng giá vắc xin mới và đầy đủ nhất tại Hệ thống trung tâm tiêm chủng VNVC,
-            giá vắc xin cạnh tranh, cam kết bình ổn giá ngay trong thời điểm khan hiếm.'
-            link='/'/>
-          </Grid>
+            bảo vệ bé trước nhiều bệnh truyền nhiễm nguy hiểm."
+            link="/"
+          />
         </Grid>
-      </Container>
-      <Footer />
-    </>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image2}
+            title="Những điều cần biết trước khi tiêm"
+            desc="Những điều cần biết trước khi tiêm giúp bác sĩ phát hiện bất thường,
+            tạm hoãn tiêm hoặc không tiêm một loại vắc xin nào đó,
+            đảm bảo an toàn cho trẻ khi đi tiêm chủng."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image3}
+            title="Những điều cần biết sau khi tiêm"
+            desc="Hướng dẫn chi tiết những điều cần biết sau khi tiêm chủng,
+            những phương pháp giúp phụ huynh theo dõi và chăm sóc trẻ sau tiêm,
+            đảm bảo an toàn tiêm chủng cho trẻ."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image4}
+            title="Các loại vắc xin cho trẻ em"
+            desc="VNVC có đủ các loại vắc xin cho trẻ em, được nhập khẩu từ nước ngoài của các hãng sản xuất uy tín,
+            nổi tiếng Thế giới, và số ít các vắc xin được sản xuất tại Việt Nam."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image5}
+            title="Gói vắc xin cho trẻ em"
+            desc="Gói vắc xin cho trẻ em đảm bảo có đầy đủ những loại vắc xin đã cam kết trong gói - ngay cả khi tình trạng khan hiếm vắc xin xảy ra,
+            cùng nhiều quyền lợi đặc biệt khác."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image6}
+            title="Gói vắc xin cho trẻ tiền học đường"
+            desc="Gói vắc xin cho trẻ tiền học đường giúp bố mẹ chủ động phòng ngừa nhiều bệnh truyền nhiễm nguy hiểm cho trẻ,
+            giúp trẻ được bảo vệ tối đa và phát triển toàn diện."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image7}
+            title="Quy trình tiêm chủng tại VNVC"
+            desc="Quy trình tiêm chủng tại VNVC khoa học, khép kín gồm 5 bước, giúp khách hàng tiêm chủng an toàn,
+            thuận tiện, nhanh chóng, tiết kiệm thời gian, không lo chờ đợi."
+            link="/"
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <VaccinationCard
+            avt={image8}
+            title="Bảng giá vắc xin"
+            desc="Cập nhật bảng giá vắc xin mới và đầy đủ nhất tại Hệ thống trung tâm tiêm chủng VNVC,
+            giá vắc xin cạnh tranh, cam kết bình ổn giá ngay trong thời điểm khan hiếm."
+            link="/"
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
