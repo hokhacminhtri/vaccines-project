febb02d48361ce227ae45891596ecf13eb00f69a
Thọ Đinh Quang <quangtho991@gmail.com>
2022-06-19 02:20:32 +0700

    cache data

--- server/src/controllers/vaccine-package.controller.js
+++ server/src/controllers/vaccine-package.controller.js
@@ -1,6 +1,8 @@
 const VaccinePackage = require('../models/mongoose/vaccine-packages.model');
 const { mongoosePaginate } = require('../mongoose-paginate');
 const DEFAULT = require('../constants/default');
+const redis = require('redis');
+const client = redis.createClient(6279);
 
 exports.getAllPackages = async (req, res) => {
   try {
@@ -17,14 +19,24 @@ exports.getPackageList = async (req, res) => {
   try {
     let page = req.query?.page || 1;
     const { select = '', sort = 'price' } = req.query;
+    const key = 'vaccinepakagelist' + page + select + sort;
 
+    client.get(key, function (err, data) {
+      if (err) {
+        console.error('Function getVaccines Error: ', error);
+        return res.status(500).json();
+      }
+      if (data != null) {
+        return JSON.parse(data);
+      }
+    });
     const docs = await mongoosePaginate(
       VaccinePackage,
       {},
       { page: Number(page), pageSize: DEFAULT.PAGE_SIZE },
       { sort, select },
     );
-
+    client.setex(key, 3600, JSON.stringify(docs));
     return res.status(200).json(docs);
   } catch (error) {
     console.error('Function getVaccines Error: ', error);
@@ -35,13 +47,42 @@ exports.getPackageList = async (req, res) => {
 exports.getPackageInfo = async (req, res) => {
   try {
     const { packageId } = req.params;
+    const key = 'packageinfo' + packageId;
+    client.get(key, function (err, data) {
+      if (err) {
+        console.error('Function getVaccines Error: ', error);
+        return res.status(500).json();
+      }
+      if (data != null) return JSON.parse(data);
+    });
     const package = await VaccinePackage.findOne({ _id: packageId }).populate({
       path: 'vaccines.vaccineId',
       select: 'name country price concept avt',
     });
+    client.setex(key, 3600, JSON.stringify(package));
     return res.status(200).json(package);
   } catch (error) {
     console.error('Function getPackageInfo Error: ', error);
+    return res.status(500).json();
+  }
+};
+
+exports.getVaccinesPackageDetail = async (req, res) => {
+  try {
+    const { categoryId } = req.params;
+    const vacincePackages = await VaccinePackage.aggregate([
+      {
+        $lookup: {
+          from: 'vaccines',
+          localField: 'category.categoryId',
+          foreignField: '_id',
+          as: 'vaccineDetail',
+        },
+      },
+    ]);
+    res.status(200).json(vacincePackages);
+  } catch (error) {
+    console.error('Function getVaccinesPackageDetail Error: ', error);
     return res.status(500).json();
   }
 };
