acefbe3772073634a753e9439be9689f63e13e79
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-06-19 14:49:41 +0700

    Server: Add redis to cache vaccine list API

---
+++ server/src/constants/redis-key.js
@@ -0,0 +1,8 @@
+module.exports = {
+  REDIS_KEY: {
+    VACCINE_LIST: 'vaccine-list',
+  },
+  REDIS_TTL: {
+    VACCINE_LIST: 600, // 10 minutes
+  },
+};
--- server/src/controllers/vaccine.controller.js
+++ server/src/controllers/vaccine.controller.js
@@ -1,6 +1,8 @@
 const Vaccine = require('../models/mongoose/vaccines.model');
 const { mongoosePaginate } = require('../mongoose-paginate');
 const DEFAULT = require('../constants/default');
+const { REDIS_KEY, REDIS_TTL } = require('../constants/redis-key');
+const { createClient } = require('redis');
 
 exports.getAllVaccines = async (req, res) => {
   try {
@@ -17,17 +19,32 @@ exports.getVaccineList = async (req, res) => {
   try {
     let page = req.query?.page || 1;
     const { select = '', sort = 'price' } = req.query;
+    const redisClient = createClient();
 
-    const docs = await mongoosePaginate(
-      Vaccine,
-      {},
-      { page: Number(page), pageSize: DEFAULT.PAGE_SIZE },
-      { sort, select },
-    );
+    await redisClient.connect();
+    const redisKey = `${REDIS_KEY.VACCINE_LIST}-${page}-${select}-${sort}`;
+    const cachedDocs = await redisClient.get(redisKey);
+    let vaccines = {};
 
-    return res.status(200).json(docs);
+    if (cachedDocs) {
+      vaccines = JSON.parse(cachedDocs);
+    } else {
+      vaccines = await mongoosePaginate(
+        Vaccine,
+        {},
+        { page: Number(page), pageSize: DEFAULT.PAGE_SIZE },
+        { sort, select },
+      );
+      await redisClient.set(redisKey, JSON.stringify(vaccines), {
+        EX: REDIS_TTL.VACCINE_LIST,
+      });
+    }
+
+    await redisClient.disconnect();
+    return res.status(200).json(vaccines);
   } catch (error) {
     console.error('Function getVaccines Error: ', error);
+    await redisClient.disconnect();
     return res.status(500).json();
   }
 };
